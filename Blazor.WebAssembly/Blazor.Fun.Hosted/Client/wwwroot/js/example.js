61609980b02171ee33783db6232257e9aaede621
Kate Zorina <kateryna@zwapgrid.com>
2020-02-23 20:09:28 +0200

    Add C# from JS interop example

--- Blazor.WebAssembly/Blazor.Fun.Hosted/Client/wwwroot/js/example.js
+++ Blazor.WebAssembly/Blazor.Fun.Hosted/Client/wwwroot/js/example.js
@@ -1,3 +1,9 @@
 ﻿window.multiplyNumber = function(number) {
   return number * 2;  
 };
+
+window.multiplyByFive = function() {
+  DotNet.invokeMethodAsync('Blazor.Fun.Hosted.Client', 'MultiplyNumberByFive', 5).then(res => {
+    alert(res);
+  })
+};
