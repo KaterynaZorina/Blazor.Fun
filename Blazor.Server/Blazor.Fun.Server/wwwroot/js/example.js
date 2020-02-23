af7bae7c79cc9929be68576787b8e74385b17689
Kate Zorina <kateryna@zwapgrid.com>
2020-02-23 20:24:54 +0200

    Add js interop component to server app

---
+++ Blazor.Server/Blazor.Fun.Server/wwwroot/js/example.js
@@ -0,0 +1,9 @@
+﻿window.multiplyNumber = function(number) {
+  return number * 2;  
+};
+
+window.multiplyByFive = function() {
+  DotNet.invokeMethodAsync('Blazor.Fun.Server', 'MultiplyNumberByFive', 5).then(res => {
+    alert(res);
+  })
+};
